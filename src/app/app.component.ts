72ff08e413edd4391608466e6f1fca19306ff528
Zhen Ee <zheneewong@gmail.com>
2021-10-14 13:01:40 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo List';
+
+  list:any[] = [];
+  addTask(item:string){
+    this.list.push({id:this.list.length, name:item});
+    console.warn(this.list.length);
+  }
+
+  removeTask(id:number){
+    //console.warn(id);
+    this.list=this.list.filter(item=>item.id != id);
+  }
+}
